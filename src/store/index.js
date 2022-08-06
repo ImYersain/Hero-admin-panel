b1efa44fbca9f547923696c998dfb68c1965c86b
Yersain <71192180+ImYersain@users.noreply.github.com>
2022-08-07 01:36:00 +0200

    Redux Toolkit (configureStore)

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,27 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'; 
 import heroesReducer from '../reducers/heroesReducer';
 import filtersReducer from '../reducers/filtersReducer';
-import ReduxThunk from 'redux-thunk';
 
-const rootReducer = combineReducers({
-    heroes: heroesReducer,
-    filters: filtersReducer
+
+// const rootReducer = combineReducers({
+//     heroes: heroesReducer,
+//     filters: filtersReducer
+// })
+
+// const store = createStore(rootReducer,                    вариант создания стора на чистом redux (снизу на toolkit-х)
+//     compose(
+//         applyMiddleware(ReduxThunk),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     ));
+
+
+const store = configureStore({
+    reducer: {
+        heroes: heroesReducer,
+        filters: filtersReducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'production'
 })
-
-const store = createStore(rootReducer, 
-    compose(
-        applyMiddleware(ReduxThunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
 
 export default store;
